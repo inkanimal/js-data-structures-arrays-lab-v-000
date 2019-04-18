689033f51874d62c5db4a9974994de7bd6f207b1
geoffrey woodcock <theatomproject@gmail.com>
2019-04-18 18:15:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"];
 
-funct destructivelyAppendDriver(name){
+function destructivelyAppendDriver(name){
+  drivers.push()
   
 }
